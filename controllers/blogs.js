045d5bb8b494b2dee462f10cd56dfe0c35b379dd
David Puerta Martos <dpuerta@usal.es>
2021-11-29 18:46:15 +0100

    4.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/Blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -9,8 +10,20 @@ blogsRouter.get('/', async (request, response) => {
  
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  
+  const user = await User.findById(body.userId)
+  
+  const blog = new Blog({
+    title: request.body.title,
+    author: user._id,
+    url: request.body.url,
+    likes: request.body.likes
+  })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.json(savedBlog)
 })
 
