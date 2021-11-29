244d1c782cbfdfa6bdcbd972210f74d227eefde9
David Puerta Martos <dpuerta@usal.es>
2021-11-29 19:24:18 +0100

    4.16

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,13 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
   const body = request.body
+  
+  if (!body.password){
+    return response.status(400).json({error: 'password missing'})
+  }
+  else if (body.password.length<3){
+    return response.status(400).json({error: 'password length must be 3 or more'})
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
@@ -23,5 +30,5 @@ usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
     response.json(users)
   })
-  
+
 module.exports = usersRouter
--- tests/api.test.js
+++ tests/api.test.js
@@ -4,6 +4,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [ { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
 { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 }, 
@@ -173,9 +174,8 @@ describe('update a blog', () => {
         const filteredBlogs = blogsAtEnd.filter(blog => blog.title===blogToUpdate.title)
         expect(filteredBlogs[0].likes).toBe(69)
     })
-})    
-
-
+})
+   
   
 afterAll(() => {
 mongoose.connection.close()
