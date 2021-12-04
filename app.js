d22f27b73509fdd703f741453c718621594fa759
David Puerta Martos <dpuerta@usal.es>
2021-12-04 18:33:24 +0100

    5.21

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -28,6 +28,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.errorHandler)
 
